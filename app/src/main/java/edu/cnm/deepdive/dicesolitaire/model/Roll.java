434f095805a78eb61f234af7e1086c349f88772a
Nick Bennett <nick@nickbenn.com>
2020-06-11 09:07:33 -0600

    Refactor

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/model/Roll.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/model/Roll.java
@@ -5,21 +5,18 @@ import java.util.Random;
 
 public class Roll {
 
-  public static final int NUM_DICE = 5;
-  public static final int NUM_FACES = 6;
-
   private final int[] dice;
 
   public Roll(Random rng) {
-    dice = new int[NUM_DICE];
-    for (int i = 0; i < NUM_DICE; i++) {
-      dice[i] = 1 + rng.nextInt(NUM_FACES);
+    dice = new int[Game.NUM_DICE];
+    for (int i = 0; i < dice.length; i++) {
+      dice[i] = 1 + rng.nextInt(Game.NUM_FACES);
     }
     Arrays.sort(dice);
   }
 
   public int[] getDice() {
-    return Arrays.copyOf(dice, NUM_DICE);
+    return Arrays.copyOf(dice, dice.length);
   }
 
 }
